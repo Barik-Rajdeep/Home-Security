10aaa699df9886548a7c62c8fb5ed5efb36fa27e
Barik-Rajdeep <rajdeepbarik5@gmail.com>
2024-08-03 20:18:18 +0530

    first commit

---
+++ security.py
@@ -0,0 +1,78 @@
+import cv2
+import time
+import datetime
+import pygame
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+def play_buzzer():
+    frequency = 2500  # Set frequency to 2500 Hertz
+    duration = 5000  # Set duration to 5000 ms == 5 second
+    pygame.mixer.init()
+    pygame.mixer.music.load("Attention Boss so 1.wav")
+    pygame.mixer.music.play()
+while True:
+    _, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
+
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started Recording!")
+            play_buzzer()  # Play buzzer when recording starts
+
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print('Stop Recording!')
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+
+    if detection:
+        out.write(frame)
+
+    # Face detection
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(frame, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    # Full body detection
+    bodies = body_cascade.detectMultiScale(gray, 1.1, 3)
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, 'Object', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+out.release()
+cap.release()
+cv2.destroyAllWindows()
